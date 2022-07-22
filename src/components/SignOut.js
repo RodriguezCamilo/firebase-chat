be43a7484dd633e16615e33b36f1e83b820977b1
Camilo Rodriguez <94582824+RodriguezCamilo@users.noreply.github.com>
2022-07-22 05:16:01 -0300

    Update SignOut.js

--- src/components/SignOut.js
+++ src/components/SignOut.js
@@ -3,7 +3,7 @@ import {auth} from "../firebase.js"
 
 function SignOut() {
     return (
-        <button onClick={()=> auth.signOut()} className='btn btn-dark btn-outline-danger' style={{width: "100%"}}> Desconectame mamawebo</button>
+        <button onClick={()=> auth.signOut()} className='btn btn-dark btn-outline-danger' style={{width: "100%"}}>SIGN OUT</button>
     )
 }
 
